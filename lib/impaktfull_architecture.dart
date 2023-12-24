2caafedb9980757de34f9ecd657ee8f424e3c88b
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2023-12-24 12:32:49 +0100

    feat: added provider widget to simplify provider architecture

--- lib/impaktfull_architecture.dart
+++ lib/impaktfull_architecture.dart
@@ -4,6 +4,7 @@ export 'src/util/connectivity/connectivity.dart';
 export 'src/provider/change_notifier_ex.dart';
 export 'src/widget/lifecycle/lifecycle_widget.dart';
 export 'src/widget/mixin/after_layout.dart';
+export 'src/widget/provider/provider_widget.dart';
 
 // 3rd party packages
 export 'package:connectivity_plus/connectivity_plus.dart';
