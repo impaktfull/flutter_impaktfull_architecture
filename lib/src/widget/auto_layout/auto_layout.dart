b64d8f0ebf18160d01581b6195a9c7de8a332b43
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2024-01-21 18:02:21 +0100

    feat: Added auto_layout like the figma component

---
+++ lib/src/widget/auto_layout/auto_layout.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:impaktfull_architecture/impaktfull_architecture.dart';
+
+enum AutoLayoutOrientation {
+  horizontal,
+  vertical,
+}
+
+class AutoLayout extends StatelessWidget {
+  final AutoLayoutOrientation orientation;
+  final MainAxisAlignment mainAxisAlignment;
+  final CrossAxisAlignment crossAxisAlignment;
+  final List<Widget> children;
+  final EdgeInsets padding;
+  final int spacing;
+
+  const AutoLayout({
+    required this.children,
+    required this.orientation,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+    this.crossAxisAlignment = CrossAxisAlignment.start,
+    this.padding = EdgeInsets.zero,
+    this.spacing = 0,
+    super.key,
+  });
+
+  const AutoLayout.horizontal({
+    required this.children,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+    this.crossAxisAlignment = CrossAxisAlignment.start,
+    this.padding = EdgeInsets.zero,
+    this.spacing = 0,
+    super.key,
+  }) : orientation = AutoLayoutOrientation.horizontal;
+
+  const AutoLayout.vertical({
+    required this.children,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+    this.crossAxisAlignment = CrossAxisAlignment.start,
+    this.padding = EdgeInsets.zero,
+    this.spacing = 0,
+    super.key,
+  }) : orientation = AutoLayoutOrientation.vertical;
+
+  @override
+  Widget build(BuildContext context) {
+    final childerenWithSpacing = <Widget>[];
+    for (var i = 0; i < children.length; i++) {
+      childerenWithSpacing.add(children[i]);
+      if (i < children.length - 1) {
+        childerenWithSpacing.add(Gap(spacing.toDouble()));
+      }
+    }
+
+    if (orientation == AutoLayoutOrientation.horizontal) {
+      return Row(
+        mainAxisAlignment: mainAxisAlignment,
+        crossAxisAlignment: crossAxisAlignment,
+        children: childerenWithSpacing,
+      );
+    }
+    return Column(
+      mainAxisAlignment: mainAxisAlignment,
+      crossAxisAlignment: crossAxisAlignment,
+      children: childerenWithSpacing,
+    );
+  }
+}
