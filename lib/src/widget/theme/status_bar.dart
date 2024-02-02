075b700ba0ea90eaafd740c626addb8551d468d9
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2024-02-02 22:47:12 +0100

    fix: statusbar

--- lib/src/widget/theme/status_bar.dart
+++ lib/src/widget/theme/status_bar.dart
@@ -3,35 +3,30 @@ import 'package:flutter/services.dart';
 
 class StatusBar extends StatelessWidget {
   final Widget child;
-  final bool dark;
+  final bool isDark;
 
-  const StatusBar.dark({
+  const StatusBar.isDark({
     required this.child,
     super.key,
-  }) : dark = true;
+  }) : isDark = true;
 
   const StatusBar.light({
     required this.child,
     super.key,
-  }) : dark = false;
+  }) : isDark = false;
 
   const StatusBar.custom({
     required this.child,
-    required this.dark,
+    required this.isDark,
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return AnnotatedRegion(
-      value: SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent,
-        systemNavigationBarColor: dark ? Colors.white : Colors.black,
-        statusBarBrightness: dark ? Brightness.light : Brightness.dark,
-        statusBarIconBrightness: dark ? Brightness.dark : Brightness.light,
-        systemNavigationBarIconBrightness:
-            dark ? Brightness.dark : Brightness.light,
-      ),
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: isDark
+          ? SystemUiOverlayStyle.light.copyWith(systemNavigationBarColor: Colors.transparent)
+          : SystemUiOverlayStyle.dark.copyWith(systemNavigationBarColor: Colors.transparent),
       child: child,
     );
   }
